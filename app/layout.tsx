a2263df44f36dc919ad6c205321d41c523245035
Rahul Singh <contact@rahools.com>
2023-11-29 13:04:04 +0530

    deployment bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 import { cn } from "@/lib/utils"
 import { ThemeProvider } from "@/components/theme-provider"
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
